1b896c6c932feb993d1cea687ad45fc1ff9335fb
Imtiaz Ahammed Nafiz <93529122+TechLead-G@users.noreply.github.com>
2022-02-23 02:59:22 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 
+const mobile_click = document.querySelector("#Mobile_Menu");
+
+mobile_click.addEventListener("click", function() {
+
+    this.querySelector("i").classList.toggle("fa-times");
+
+    const mobile_menu_wraper = document.querySelector(".navigation-bar");
+
+    mobile_menu_wraper.classList.toggle("show_main_menu");
+
+})
